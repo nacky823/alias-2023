f36163d62f10446c2ee8fc783d3042f6a8de3f59
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2023-02-05 18:43:21 +0900

    up

--- Core/Inc/logger.hpp
+++ Core/Inc/logger.hpp
@@ -1,13 +1,15 @@
 #ifndef LOGGER_HPP_
 #define LOGGER_HPP_
 
+#define MAX_LOG_INDEX 6200
+
 class Logger
 {
 private:
     /* Data at 10mm intervals. */
-    uint8_t various_flags_[6200]; //  6.2 [kByte]
-    int16_t gyro_data_yaw_[6200]; // 12.4 [kByte]
-    float excess_distance_[6200]; // 24.8 [kByte]
+    uint16_t various_data_[MAX_LOG_INDEX]; // 12.4 [kByte]
+    int16_t gyro_data_yaw_[MAX_LOG_INDEX]; // 12.4 [kByte]
+    float excess_distance_[MAX_LOG_INDEX]; // 24.8 [kByte]
 
 public:
     Logger();
--- Core/Src/logger.cpp
+++ Core/Src/logger.cpp
@@ -2,3 +2,34 @@
 
 Logger::Logger()
 
+void Log::Logging(uint8_t interrupt_end)
+{
+    float distance_10mm = encoder.GetDistance10mm();
+
+    if(distance_10mm > FORMAL_10MM)
+    {
+        static uint16_t log_index = 0;
+        static uint8_t pre_goal_cnt   = side_sensor.GetGoalMarkerCount();
+        static uint8_t pre_corner_cnt = side_sensor.GetCornerMarkerCount();
+        static uint8_t pre_cross_cnt  = side_sensor.GetCrossLineCount();
+        uint8_t goal_cnt   = side_sensor.GetGoalMarkerCount();
+        uint8_t corner_cnt = side_sensor.GetCornerMarkerCount();
+        uint8_t cross_cnt  = side_sensor.GetCrossLineCount();
+        uint16_t various_buff = 0;
+
+        if(interrupt_end == 1)           various_buff |= 0x0008;
+        if(pre_goal_cnt != goal_cnt)     various_buff |= 0x0004;
+        if(pre_corner_cnt != corner_cnt) various_buff |= 0x0002;
+        if(pre_cross_cnt != cross_cnt)   various_buff |= 0x0001;
+    
+        pre_goal_cnt   = goal_cnt;
+        pre_corner_cnt = corner_cnt;
+        pre_cross_cnt  = cross_cnt;
+
+        various_data_[log_index]    = various_buff;
+        gyro_data_yaw_[log_index]   = iim_42652.GyroZLeft();
+        excess_distance_[log_index] = distance_10mm - FORMAL_10MM;
+    
+        if(log_index < MAX_LOG_INDEX) log_index++;
+    }
+}

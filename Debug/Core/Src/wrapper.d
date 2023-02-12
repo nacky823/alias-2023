d33b3693100380985a94827928c2791bf9df5136
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2023-02-12 19:31:07 +0900

    fix

--- Debug/Core/Src/wrapper.d
+++ Debug/Core/Src/wrapper.d
@@ -31,15 +31,14 @@ Core/Src/wrapper.o: ../Core/Src/wrapper.cpp ../Core/Inc/wrapper.hpp \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
- ../Core/Inc/encoder.hpp ../Core/Inc/declare_extern.h ../Core/Inc/macro.h \
+ ../Core/Inc/declare_extern.h ../Core/Inc/macro.h ../Core/Inc/encoder.hpp \
  ../Core/Inc/flash.hpp ../Core/Inc/iim_42652.hpp ../Core/Inc/led.hpp \
  ../Core/Inc/line_sensor.hpp ../Core/Inc/main.h \
  ../Core/Inc/line_trace.hpp ../Core/Inc/line_sensor.hpp \
  ../Core/Inc/logger.hpp ../Core/Inc/encoder.hpp ../Core/Inc/flash.hpp \
  ../Core/Inc/iim_42652.hpp ../Core/Inc/side_sensor.hpp \
  ../Core/Inc/motor.hpp ../Core/Inc/rotary_switch.hpp \
- ../Core/Inc/side_sensor.hpp ../Core/Inc/velocity_control.hpp \
- ../Core/Inc/macro.h ../Core/Inc/declare_extern.h
+ ../Core/Inc/side_sensor.hpp ../Core/Inc/velocity_control.hpp
 
 ../Core/Inc/wrapper.hpp:
 
@@ -107,11 +106,11 @@ Core/Src/wrapper.o: ../Core/Src/wrapper.cpp ../Core/Inc/wrapper.hpp \
 
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
 
-../Core/Inc/encoder.hpp:
-
 ../Core/Inc/declare_extern.h:
 
 ../Core/Inc/macro.h:
+
+../Core/Inc/encoder.hpp:
 
 ../Core/Inc/flash.hpp:
 
@@ -144,7 +143,3 @@ Core/Src/wrapper.o: ../Core/Src/wrapper.cpp ../Core/Inc/wrapper.hpp \
 ../Core/Inc/side_sensor.hpp:
 
 ../Core/Inc/velocity_control.hpp:
-
-../Core/Inc/macro.h:
-
-../Core/Inc/declare_extern.h:

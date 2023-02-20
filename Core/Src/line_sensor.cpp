46bb2865ce4cdd57d065715d03aa73fa65a289d2
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2023-02-21 02:57:11 +0900

    edit merge sort

--- Core/Src/line_sensor.cpp
+++ Core/Src/line_sensor.cpp
@@ -54,56 +54,24 @@ void LineSensor::MergeSort(uint16_t array[], uint8_t first_index, uint8_t last_i
     MergeSort(array, first_index, middle);
     MergeSort(array, middle+1, last_index);
 
-    for(i = middle; i >= first_index; i--) temp_array[i] = array[i];
+    for(i = middle; i >= first_index; i--)
+    {
+        temp_array[i] = array[i];
+    }
     for(i = middle+1; i <= last_index; i++)
     {
         temp_array[last_index - (i - (middle+1))] = array[i];
     }
+
     left  = first_index;
     right = last_index;
+
     for(i = first_index; i <= last_index; i++)
     {
         if(temp_array[left] < temp_array[right]) array[i] = temp_array[left++];
-        else array[i] = temp_array[right--];
+        else                                     array[i] = temp_array[right--];
     }
 }
-
-/*
-void LineSensor::UpdateAdcValues()
-{
-    uint8_t i, j, emergency = 1;
-    uint16_t sample_array[NUMBER_OF_SAMPLE];
-    uint16_t sample_median, max, min, max_range, normal = 0;
-
-    for(i = 0; i < NUMBER_OF_ADC; i++)
-    {
-        for(j = 0; j < NUMBER_OF_SAMPLE; j++)
-        {
-            sample_array[j] = consecutive_adc_buffers_[j][i];
-        }
-        MergeSort(sample_array, 0, NUMBER_OF_SAMPLE-1);
-        sample_median = sample_array[INDEX_OF_SAMPLE_MEDIAN];
-
-#ifdef DEBUG_MODE
-        for(j = 0; j < NUMBER_OF_SAMPLE; j++) g_consecutive_buffers[i][j] = sample_array[j];
-#endif // DEBUG_MODE
-
-        if(sample_median < EMERGENCY_STOP_BORDER) emergency = 0;
-
-        max = max_adc_values_[i];
-        min = min_adc_values_[i];
-        if(min > sample_median || min == 0) min = sample_median;
-        else if(max < sample_median)        max = sample_median;
-        max_adc_values_[i] = max;
-        min_adc_values_[i] = min;
-
-        max_range = max - min;
-        if(max_range != 0) normal = MAX_VALUE * (sample_median - min) / max_range;
-        adc_values_[i] = normal;
-    }
-    emergency_stop_flag_ = emergency;
-}
-*/
 
 void LineSensor::Update()
 {

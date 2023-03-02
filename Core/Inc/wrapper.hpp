b4854c7f2f9df7eb7a1d7c1bfeb9463a96d2b355
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2023-03-02 09:35:38 +0900

    move wrapper 何かあったらひとつ前に

--- Core/Inc/wrapper.hpp
+++ Core/Inc/wrapper.hpp
@@ -7,26 +7,11 @@ extern "C" {
 
 #include "stm32f4xx_hal.h"
 #include "declare_extern.h"
-#include <stdbool.h>
 
 void Init();
-void ExternalInterrupt(uint16_t);
 void InterruptTim7();
 void InterruptTim6();
-void InterruptTim5();
 void InterruptTim2();
-void Loop();
-
-void EmergencyStop();
-float TargetVelocity(float target, float min);
-float TargetDuty(float target, float min);
-
-#ifdef DEBUG_MODE
-uint8_t FlashTest();
-void InitialTest();
-uint8_t ImuReadByte(uint8_t, char);
-void ImuWriteByte(uint8_t, uint8_t, char);
-#endif // DEBUG_MODE
 
 #ifdef __cplusplus
 }

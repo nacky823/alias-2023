9982ab56e34307402eb8959712b6a1259768b9e6
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2023-03-02 12:41:35 +0900

    up

--- Core/Inc/macro.h
+++ Core/Inc/macro.h
@@ -2,7 +2,8 @@
 #define MACRO_H_
 
 /* ----- Debug mode ----- */
-//#define DEBUG_MODE
+#define DEBUG_MODE
+#define TEST_MODE
 
 /* ----- Parameter adjustment ----- */
 #define INIT_DEBUG_MOTOR_DUTY 0.0

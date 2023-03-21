ed6b3726f2143ce5307601d0aabaeccb0a84ad8a
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2023-03-22 02:18:45 +0900

    logging 書き直し一段落

---
+++ Core/Inc/logger2.hpp
@@ -0,0 +1,8 @@
+#ifndef LOGGER2_HPP_
+#define LOGGER2_HPP_
+
+#include "encoder.hpp"
+
+
+/* Address to store error code when flash write fails. */
+#define ERROR_CODE_STORE_DISTANCE
--- Core/Src/logger2.cpp
+++ Core/Src/logger2.cpp
@@ -3,15 +3,23 @@
 void Logger2::Logging()
 {
     float distance = encoder_->GetDistanceStack();
-
     if(distance < LOGGING_CONST_DISTANCE) return;
-
     encoder_->ResetDistanceStack();
 
-    static uint16_t now_address = 0; // flash address count (0 ~ 6199)
+    uint8_t result = StoreDistanceLog(distance);
+    CheckLoggingSuccess(ERROR_CODE_STORE_DISTANCE, result);
 
+    result = StoreRadianLog();
+    CheckLoggingSuccess(ERROR_CODE_STORE_RADIAN, result);
 
+    result = StoreVariousLog();
+    CheckLoggingSuccess(ERROR_CODE_STORE_VARIOUS, result);
 
+    DistanceCorrection(distance);
+
+    LoggingAccelPosition();
+
+    logging_now_address_++;
 }
 
 bool Logger2::AssertNowaddress(uint16_t now_address)
@@ -28,7 +36,7 @@ uint8_t Logger2::StoreDistanceLog(float distance)
 
     if(!AssertNowaddress(now_address)) result = 0x01;
     else if(!flash_->CheckBlankWord(address, 1)) result = 0x02;
-    else if(!flash_->StoreFloat(address, distance, 1)) result = 0x03;
+    else if(!flash_->StoreFloat(address, &distance, 1)) result = 0x03;
 
     now_address++;
 
@@ -37,24 +45,28 @@ uint8_t Logger2::StoreDistanceLog(float distance)
 
 uint8_t Logger2::StoreRadianLog()
 {
-    static uint32_t address = HEAD_ADDRESS_BLOCK_B;
+    static uint16_t now_address = 0;
+    uint32_t address = now_address * 4 + HEAD_ADDRESS_BLOCK_B;
     uint8_t result = 0;
 
     double degree = iim_42652_->GetDegreeStackZ();
     float radian = static_cast<float>(degree * M_PI / 180.0);
     iim_42652_->ResetDegreeStackZ();
+    logging_radian_buff_ = radian;
 
-    if(!flash_->CheckBlankWord(address, 1)) result = 0x01;
-    else if(!flash_->StoreFloat(address, radian, 1)) result = 0x02;
+    if(!AssertNowaddress(now_address)) result = 0x01;
+    else if(!flash_->CheckBlankWord(address, 1)) result = 0x02;
+    else if(!flash_->StoreFloat(address, &radian, 1)) result = 0x03;
 
-    address += 4;
+    now_address++;
 
     return result;
 }
 
 uint8_t Logger2::StoreVariousLog()
 {
-    static uint32_t address = HEAD_ADDRESS_BLOCK_C;
+    static uint16_t now_address = 0;
+    uint32_t address = now_address * 2 + HEAD_ADDRESS_BLOCK_C;
     uint8_t result = 0;
 
     uint8_t corner_count = side_sensor_->GetCornerMarkerCount();
@@ -69,15 +81,16 @@ uint8_t Logger2::StoreVariousLog()
     pre_corner_count = corner_count;
     pre_cross_count  = cross_count;
 
-    if(!flash_->CheckBlankHalfword(address, 1)) result = 0x01;
-    else if(!flash_->StoreUint16(address, various, 1)) result = 0x02;
+    if(!AssertNowaddress(now_address)) result = 0x01;
+    else if(!flash_->CheckBlankHalfword(address, 1)) result = 0x02;
+    else if(!flash_->StoreUint16(address, &various, 1)) result = 0x03;
 
-    address += 4;
+    now_address++;
 
     return result;
 }
 
-uint16_t Logger2::DistanceCorrection(float distance)
+void Logger2::DistanceCorrection(float distance)
 {
     uncorrected_address_buff_ = logging_now_address_;
 
@@ -86,30 +99,95 @@ uint16_t Logger2::DistanceCorrection(float distance)
 
     if(excess_stack > LOGGING_CONST_DISTANCE)
     {
-        uint8_t result_store_distance = StoreDistanceLog(distance);
+        uint8_t result = StoreDistanceLog(distance);
+        CheckLoggingSuccess(ERROR_CODE_STORE_DISTANCE, result);
 
-        if(result_store_distance != 0)
-        {
-            SetEmergencyStopFlag();
-        }
+        excess_stack -= LOGGING_CONST_DISTANCE;
 
-
-
+        logging_now_address_++;
     }
 }
 
-void Logger2::SetEmergencyStopFlag()
+void Logger2::CheckLoggingSuccess(uint32_t address, uint8_t data)
 {
-    emergency_stop_flag_ = true;
+    if(data != 0)
+    {
+        SetEmergencyStopFlag(true);
+
+        if(flash_->StoreUint8(address, &data, 1))
+        {
+            SetSuccessEmergencyCodeStore(true);
+        }
+    }
 }
 
+void Logger2::SetEmergencyStopFlag(bool boolean)
+{
+    emergency_stop_flag_ = boolean;
+}
 
+bool Logger2::GetEmergencyStopFlag()
+{
+    return emergency_stop_flag_;
+}
 
+void Logger2::SetSuccessEmergencyCodeStore(bool boolean)
+{
+    success_emergency_code_store_ = boolean;
+}
 
+bool Logger2::GetSuccessEmergencyCodeStore()
+{
+    return success_emergency_code_store_;
+}
 
+void Logger2::LoggingAccelPosition()
+{
+    static uint16_t accel_straight_count = 0;
+    static uint8_t accel_step = 0;
 
+    if(fabs(encoder_->AngularVelocity()) < STRAIGHT_BORDER_ENCODER)
+    {
+        accel_straight_count++;
+        if(accel_straight_count >= CNT_OF_ACCEL_STEP_UP && accel_step < NUM_OF_ACCEL_STEP)
+        {
+            accel_step++;
 
+            uint32_t accel_address = uncorrected_address_buff_ - CNT_OF_ACCEL_STEP_UP;
 
+            uint8_t result = StoreAccelPosition(accel_address, accel_step);
+
+            CheckLoggingSuccess(ERROR_CODE_STORE_ACCEL, result);
+
+            accel_straight_count = 0;
+        }
+    }
+    else if(accel_step != 0)
+    {
+        for(uint8_t i = 1; i <= accel_step; i++)
+        {
+            uint32_t decel_address = (uncorrected_address_buff_-1) - (DIFF_NEXT_ACCEL_STEP * i);
+            uint8_t decel_step = i << 4;
+
+            uint8_t result = StoreAccelPosition(decel_address, decel_step);
+
+            CheckLoggingSuccess(ERROR_CODE_STORE_DECEL, result);
+        }
+        accel_step = 0;
+        accel_straight_count = 0;
+    }
+    else accel_straight_count = 0;
+}
+
+uint8_t Logger2::StoreAccelPosition(uint32_t address, uint8_t data)
+{
+    address += HEAD_ADDRESS_BLOCK_D;
+
+    if(!flash_->CheckBlankByte(address, 1)) return 0x01;
+    else if(!flash_->StoreUint8(address, &data, 1)) return 0x02;
+
+    return 0;
+}
 
 
 

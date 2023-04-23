486d287c420acc9ab522dcb63217342ea2e57072
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2023-04-24 06:53:31 +0900

    updadte

--- Core/Inc/imu.hpp
+++ Core/Inc/imu.hpp
@@ -3,7 +3,8 @@
 
 #include "iim_42652.hpp"
 
-#define
+#define RAD_LPF_RATIO  0.7
+#define CONST_RAD_CALC 0.001065 // float
 
 /* raw_imu_value (int16_t) : raw (-32768 <= raw <= 32767) [-]
  * normalization           : norm = 32768 [-]
@@ -20,11 +21,18 @@
 class Imu
 {
 private:
-
     Iim42652 *iim_42652_;
+
+    float rad_z_;
+    float rad_stack_z_;
+    bool clear_integral_error_;
 
 public:
     Imu(Iim42652 *);
+    void Update();
+    void ClearRadStackZ();
+    float GetRadStackZ();
+    float PidControl();
 
 };
 
--- Core/Src/imu.cpp
+++ Core/Src/imu.cpp
@@ -1,11 +1,58 @@
 #include "imu.hpp"
 
-Imu::Imu(Iim42652 *iim_42652)
+Imu::Imu(Iim42652 *iim_42652) : rad_z_(0), rad_stack_z_(0)
 {
     iim_42652_ = iim_42652;
 }
 
 void Imu::Update()
 {
+    int16_t raw_gyro_z_l = iim_42652_->GyroZLeft();
+    int16_t raw_gyro_z_r = iim_42652_->GyroZRight();
+    float avg_raw_gyro_z = (raw_gyro_z_l + raw_gyro_z_r) / 2.0;
+    float rad_z = avg_raw_gyro_z * CONST_RAD_CALC * TIM6_PERIOD_S;
 
+    /* low pass filter */
+    static float pre_filter = 0;
+    float filter = pre_filter * (1.0 - RAD_LPF_RATIO) + (rad_z * RAD_LPF_RATIO);
+    pre_filter = filter;
+
+    rad_z_ = filter;
+    rad_stack_z_ += filter;
+
+#ifdef DEBUG_MODE
+    g_rad_z = rad_z;
+    g_rad_stack_z = rad_stack_z_;
+#endif // DEBUG_MODE
+}
+
+void Imu::ClearRadStackZ()
+{
+    rad_stack_z_ = 0;
+}
+
+float Imu::GetRadStackZ()
+{
+    return rad_stack_z_;
+}
+
+float Imu::PidControl(float error)
+{
+    static float pre_error = error;
+    static float integral_error = 0;
+    float differential_error
+    float p, i, d;
+    
+    integral_error += (error + pre_error) / 2.0 * TIM6_PERIOD_S;
+    differential_error = (error - pre_error) / TIM6_PERIOD_S;
+
+    pre_error = error;
+
+    if(clear_integral_error_) integral_error = 0;
+
+    p = GYRO_P_GAIN * error;
+    i = GYRO_I_GAIN * integral_error;
+    d = GYRO_D_GAIN * differential_error;
+
+    return p + i + d;
 }

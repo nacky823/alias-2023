69ede72aacb5801ba9198e4d94c8494abbcc9abe
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2023-04-24 04:51:35 +0900

    add comment for culcration radian

--- Core/Inc/imu.hpp
+++ Core/Inc/imu.hpp
@@ -3,6 +3,20 @@
 
 #include "iim_42652.hpp"
 
+#define
+
+/* raw_imu_value (int16_t) : raw (-32768 <= raw <= 32767) [-]
+ * normalization           : norm = 32768 [-]
+ * sensitivity (+-2000dps) : sens = 2000 [deg/s]
+ *
+ * ans[rad] = raw[-] / norm[-] * sens[deg/s] / 180[deg] * pi[rad] * timer_period[s]
+ *
+ * constant[rad/s] = sens[deg/s] * pi[rad] / ( norm[-] * 180[deg] )
+ *                 = 0.00106526443603169529841533860381 [rad/s]
+ *
+ * ans[rad] = raw[-] * constant[rad/s] * timer_period[s]
+ */
+
 class Imu
 {
 private:
--- Core/Src/imu.cpp
+++ Core/Src/imu.cpp
@@ -5,4 +5,7 @@ Imu::Imu(Iim42652 *iim_42652)
     iim_42652_ = iim_42652;
 }
 
-void 
+void Imu::Update()
+{
+
+}

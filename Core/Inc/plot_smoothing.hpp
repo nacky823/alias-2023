0bf89661f4be1dca1528ef9f1f68eb9d1451b130
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2023-05-31 11:53:36 +0900

    add plot_smoothing.hpp

---
+++ Core/Inc/plot_smoothing.hpp
@@ -0,0 +1,13 @@
+#ifndef PLOT_SMOOTHING_HPP_
+#define PLOT_SMOOTHING_HPP_
+
+#include "stm32f4xx_hal.h"
+
+class PlotSmoothing
+{
+private:
+    void Smoothing(float *, uint16_t, uint16_t);
+
+};
+
+#endif // PLOT_SMOOTHING_HPP_

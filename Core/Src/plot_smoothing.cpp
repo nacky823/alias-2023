b62af8588209f244b08cbf4a761a29733ccaf3de
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2023-05-31 11:47:56 +0900

    add plot_smoothing.cpp

---
+++ Core/Src/plot_smoothing.cpp
@@ -0,0 +1,36 @@
+#include "plot_smoothing.hpp"
+
+void PlotSmoothing::Smoothing(float *data_addr, uint16_t data_size, uint16_t num_of_adjacent)
+{
+    float cp_data[data_size];
+    uint16_t i, j, max;
+    int16_t min;
+    double sum;
+    float avg;
+
+    for(i = 0; i < data_size; i++) cp_data[i] = 0;
+
+    for(i = 0; i < data_size; i++)
+    {
+        sum = 0;
+        min = i - num_of_adjacent;
+        max = i + num_of_adjacent;
+        for(j = min; j <= max; j++)
+        {
+            if(j < 0 || j >= data_size) continue;
+            sum += *(data_addr + j);
+        }
+        if(i < num_of_adjacent || i >= (data_size - num_of_adjacent))
+        {
+            cp_data[i] = *(data_addr + i);
+            continue;
+        }
+        avg = (float)sum / (num_of_adjacent * 2 + 1);
+        cp_data[i] = avg;
+    }
+
+    for(i = 0; i < data_size; i++)
+    {
+        *(data_addr + i) = cp_data[i];
+    }
+}

c5769c38a1a9202ec10358738b31312bbc232a37
nacky_wsl_ubuntu22 <youjiyongmu4@gmail.com>
2023-06-02 03:24:04 +0900

    update plot_smoothing

--- Core/Src/plot_smoothing.cpp
+++ Core/Src/plot_smoothing.cpp
@@ -1,12 +1,25 @@
 #include "plot_smoothing.hpp"
 
+PlotSmoothing::PlotSmoothing
+: now_address_(0)
+, radian_stack_(0)
+{
+
+}
+
+void PlotSmoothing::SetNowAddress(uint32_t address)
+{
+    now_address_ = address;
+}
+
 void PlotSmoothing::StackRadian()
 {
-    uint32_t address = SECTOR_3_ADDRESS_HEAD;
+    uint32_t address = now_address_;
     uint32_t int_data = 555555;
     float radian = flash_->Load(&int_data, address, 4);
-    // now koko
 
+    radian_stack_ += radian;
+    now_address_ += 4;
 }
 
 void PlotSmoothing::Smoothing(float *data_addr, uint16_t data_size, uint16_t num_of_adjacent)
